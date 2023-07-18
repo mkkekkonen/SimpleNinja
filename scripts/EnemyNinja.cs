d0d9edd47e93065f307a17e693d53562ff69fd01
Maija Kekkonen <mkkekkonen@protonmail.com>
2023-07-18 23:52:11 +0300

    - add enemy ninja scene with platform detection/direction change - add an enemy to the first level

---
+++ scripts/EnemyNinja.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System;
+
+public partial class EnemyNinja : CharacterBody2D
+{
+  public const float Speed = 150.0f;
+  public const float JumpVelocity = -400.0f;
+
+  public Vector2 Direction = new Vector2(1, 0);
+
+  // Get the gravity from the project settings to be synced with RigidBody nodes.
+  public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+  public override void _Process(double delta)
+  {
+    base._Process(delta);
+
+    var sprite = GetNode<Sprite2D>("Sprite2D");
+
+    if (Velocity.X < 0 && !sprite.FlipH)
+    {
+      sprite.FlipH = true;
+    }
+    else if (Velocity.X > 0 && sprite.FlipH)
+    {
+      sprite.FlipH = false;
+    }
+  }
+
+  public override void _PhysicsProcess(double delta)
+  {
+    Vector2 velocity = Velocity;
+
+    // Add the gravity.
+    if (!IsOnFloor())
+      velocity.Y += gravity * (float)delta;
+
+    HandleDirection();
+
+    // Get the input direction and handle the movement/deceleration.
+    // As good practice, you should replace UI actions with custom gameplay actions.
+    if (Direction != Vector2.Zero)
+    {
+      velocity.X = Direction.X * Speed;
+    }
+    else
+    {
+      velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+    }
+
+    Velocity = velocity;
+    MoveAndSlide();
+  }
+
+  private void HandleDirection()
+  {
+    var direction = Direction;
+
+    var rayCastLeft = GetNode<RayCast2D>("RayCast2DLeft");
+    var rayCastRight = GetNode<RayCast2D>("RayCast2DRight");
+
+    var endOfPlatform = rayCastLeft.GetCollider() == null
+      || rayCastRight.GetCollider() == null;
+
+    if (IsOnWall() || endOfPlatform)
+    {
+      Direction = direction * new Vector2(-1, 0);
+    }
+  }
+}
